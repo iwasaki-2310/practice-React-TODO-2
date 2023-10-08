5af94a12ef9687018cf2917a44814b2b041ec8e0
iwasaki2310 <118096073+iwasaki-2310@users.noreply.github.com>
2023-10-08 10:38:27 +0000

    React 17.0.2の環境整備

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import "./styles.css";
+import React, { useEffect, useState } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+export const App = () => {};
